29fd540adbac09387755cb9d750523a2f3782124
mert <mert.ma346@gmail.com>
2022-08-23 21:26:26 +0300

    tableView için kullanılması gereken iki fonksiyonu ekledik.

--- tableView/ViewController.swift
+++ tableView/ViewController.swift
@@ -7,13 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource   { //tableview deki iki fonksiyonu kullanabilmek için bu ikisini eklememiz gerekiyor.
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // NumberOfRowsInsection -> kaç tane row olacağını belirler.
+    // cellForRow atIndexPath -> hücrenin içerisinde neler gösterilecek.
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
 }
 
